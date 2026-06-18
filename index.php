ed83ed401b0c6f73aca4a697d4f00466fe053cad
Hanbragher <hanbragher@gmail.com>
2026-06-18 23:09:01 +0400

    111

---
+++ index.php
@@ -0,0 +1,435 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Grapharet Touch</title>
+
+    <meta name="robots" content="noindex, nofollow" />
+
+    <link rel="shortcut icon" href="logo40px.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" >
+    <!-- Fonts -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome.min.css">
+    <!-- Icon -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/simple-line-icons.css">
+    <!-- Slicknav -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
+    <!-- Menu CSS -->
+   <!-- <link rel="stylesheet" type="text/css" href="/assets/css/menu_sideslide.css">-->
+    <!-- Slider CSS -->
+    <!--<link rel="stylesheet" type="text/css" href="/assets/css/slide-style.css">-->
+    <!-- Nivo Lightbox -->
+    <link rel="stylesheet" type="text/css" href="assets/css/nivoLightbox.css" >
+    <!-- Animate -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <!-- Main Style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <!-- Responsive Style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+      <!-- Yandex.Metrika counter -->
+      <script type="text/javascript" >
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(102979166, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+          });
+      </script>
+      <noscript><div><img src="https://mc.yandex.ru/watch/102979166" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+      <!-- /Yandex.Metrika counter -->
+
+  </head>
+  <body>
+
+
+    <!-- Header Area wrapper Starts -->
+    <header id="header-wrap">
+      <!-- Navbar Start -->
+      <nav class="navbar navbar-expand-lg fixed-top scrolling-navbar indigo">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+              <span class="icon-menu"></span>
+              <span class="icon-menu"></span>
+              <span class="icon-menu"></span>
+            </button>
+            <a href="index.php" class="navbar-brand">
+              <img src="/assets/img/logo60px1.png" alt="">
+            </a>
+          </div>
+
+          <div class="collapse navbar-collapse" id="main-navbar">
+            <ul class="onepage-nev navbar-nav mr-auto w-100 justify-content-end clearfix">
+              <li class="nav-item active">
+                <a class="nav-link" href="#hero-area">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#about">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#services">
+                  Services
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#portfolios">
+                  Work
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+
+        <!-- Mobile Menu Start -->
+        <ul class="onepage-nev mobile-menu">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <!--<li>
+            <a href="#resume">Resume</a>
+          </li>-->
+          <li>
+            <a href="#portfolios">Work</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <!-- Mobile Menu End -->
+      </nav>
+      <!-- Navbar End -->
+
+      <!-- Hero Area Start -->
+      <div id="hero-area" class="hero-area-bg">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 text-center">
+              <div class="contents">
+                <h5 class="script-font wow fadeInUp" data-wow-delay="0.2s">Hi This is</h5>
+                <h2 class="head-title wow fadeInUp" data-wow-delay="0.4s">Grapharet</h2>
+                <p class="script-font wow fadeInUp" data-wow-delay="0.6s">We create vector and raster graphics</p>
+                <p class="script-font wow fadeInUp" data-wow-delay="0.6s">bring it in real life</p>
+
+                <div class="header-button wow fadeInUp" data-wow-delay="1s">
+                  <a href="http://m.me/grapharet" class="btn btn-common">
+                    <i class="icon-social-facebook"></i> Send a message
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Hero Area End -->
+
+    </header>
+    <!-- Header Area wrapper End -->
+
+    <!-- About Section Start -->
+    <section id="about" class="section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="img-thumb wow fadeInLeft" data-wow-delay="0.3s">
+              <img class="img-fluid" src="/assets/img/about/about-1.png" alt="">
+            </div>
+          </div> 
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="profile-wrapper wow fadeInRight" data-wow-delay="0.3s">
+              <h3>Hello!</h3>
+              <p style="font-size:17px">We are professional team, who creating, modify and edit media graphics around the world. With our skills, you can add the necessary brick to your business and make it more complete. </p>
+              <!--<div class="about-profile">
+                <ul class="admin-profile">
+                  <li>
+                    <span class="pro-title"> Name </span> <span class="pro-detail">Tom Saulnier</span>
+                  </li>
+                </ul>
+              </div>-->
+              <!--<a href="#" class="btn btn-common"><i class="icon-paper-clip"></i> Download Resume</a>
+              <a href="#" class="btn btn-danger"><i class="icon-speech"></i> Contact Me</a>-->
+            </div>
+          </div>   
+        </div>
+      </div>
+    </section>
+    <!-- About Section End -->
+
+    <!-- Services Section Start -->
+    <section id="services" class="services section-padding">
+      <h2 class="section-title wow flipInX" data-wow-delay="0.4s">What we do</h2>
+      <div class="container">
+        <div class="row">
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-3 col-xs-12">
+            <div class="services-item wow fadeInDown" data-wow-delay="0.3s">
+              <div class="icon">
+                <i class="icon-vector"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#portfolios" class="filter" data-filter="all">Vector Graphic</a></h3>
+                <p>Scalable digital images created with mathematical equations.</p>
+              </div>
+            </div>
+          </div>
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-3 col-xs-12">
+            <div class="services-item wow fadeInDown" data-wow-delay="0.6s">
+              <div class="icon">
+                <i class="icon-layers"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#portfolios" class="filter" data-filter="all">Raster Graphic</a></h3>
+                <p>Digital image created with pixels, resolution determines quality.</p>
+              </div>
+            </div>
+          </div>
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-3 col-xs-12">
+            <div class="services-item wow fadeInDown" data-wow-delay="0.9s">
+              <div class="icon">
+                <i class="icon-picture"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#portfolios" class="filter" data-filter="all">Photography</a></h3>
+                <p>Art of capturing images with a camera.</p>
+              </div>
+            </div>
+          </div>
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-3 col-xs-12">
+            <div class="services-item wow fadeInDown" data-wow-delay="1.2s">
+              <div class="icon">
+                <i class="icon-printer"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#portfolios" class="filter" data-filter="all">Print</a></h3>
+                <p>We offer printing services in various methods and materials.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Services Section End -->
+
+    <!-- Portfolio Section -->
+    <section id="portfolios" class="section-padding">
+      <!-- Container Starts -->
+      <div class="container">        
+        <h2 class="section-title wow flipInX" data-wow-delay="0.4s">Our Works</h2>
+        <div class="row">          
+          <div class="col-md-12">
+            <!-- Portfolio Controller/Buttons -->
+            <div class="controls text-center">
+
+              <a class="filter active btn btn-common" data-filter="all">
+                All 
+              </a>
+
+                <?php
+                    //$categories = ['vector', 'raster', 'photography', 'print'];
+                    $categories = ['New Boxes'=>'boxes', 'designs'=>'designs', 'logos'=>'logos', 'photography'=>'photography'];
+                    foreach ($categories as $category_name=>$category){
+                        echo '<a class="filter btn btn-common" data-filter=".'.$category.'">'.$category_name.'</a>';
+                    }
+                ?>
+            </div>
+            <!-- Portfolio Controller/Buttons Ends-->
+          </div>
+
+          <!-- Portfolio Recent Projects -->
+          <div id="portfolio" class="row wow fadeInDown" data-wow-delay="0.4s">
+
+              <?php
+                  $artworks = array_diff(scandir('assets/portfolio/files'), array('..', '.'));
+                  foreach($artworks as $artwork){
+                      $artwork_array = explode( ';', str_replace('.jpg', "", $artwork));
+                      $artwork_caption = $artwork_array[0];
+                      $artwork_categories = implode(" ", array_slice($artwork_array, 1));
+
+                      if(!empty($artwork)){
+                          echo '<div class="col-sm-6 col-md-4 col-lg-4 col-xl-4 mix '.$artwork_categories.'">
+                                  <div class="portfolio-item">
+                                    <div class="shot-item">
+                                      <img src="assets/portfolio/files/'.$artwork.'" alt="'.$artwork_caption.'" title="'.$artwork_caption.'"/>
+                                      <div class="overlay">
+                                        <div class="icons">
+                                          <a class="lightbox preview" href="assets/portfolio/files/'.$artwork.'" title="'.$artwork_caption.'">
+                                            <i class="icon-eye"></i>
+                                          </a>
+                                        </div>
+                                      </div>
+                                    </div>               
+                                  </div>
+                                </div>';
+                      };
+                  };
+              ?>
+          </div>
+        </div>
+      </div>
+      <!-- Container Ends -->
+    </section>
+    <!-- Portfolio Section Ends --> 
+
+    <!-- Counter Area Start-->
+    <section class="counter-section section-padding">
+      <div class="container">
+        <div class="row">
+            <!-- Counter Item -->
+            <div class="col-md-3 col-sm-6 work-counter-widget text-center">
+                <div class="counter wow fadeInDown" data-wow-delay="0.3s">
+                    <div class="icon"><i class="icon-user-following"></i></div>
+                    <!--<div class="counterUp">250</div>-->
+                    <p>Individual Approach</p>
+                </div>
+            </div>
+
+            <!-- Counter Item -->
+            <div class="col-md-3 col-sm-6 work-counter-widget text-center">
+                <div class="counter wow fadeInDown" data-wow-delay="0.6s">
+                    <div class="icon"><i class="icon-target"></i></div>
+                    <!--<div class="counterUp">950</div>-->
+                    <p>Personalized Touch</p>
+                </div>
+            </div>
+
+
+
+          <!-- Counter Item -->
+          <div class="col-md-3 col-sm-6 work-counter-widget text-center">
+            <div class="counter wow fadeInDown" data-wow-delay="0.9s">
+              <div class="icon"><i class="icon-diamond"></i></div>
+              <!--<div class="counterUp">150</div>-->
+              <p>Unique Designs</p>
+            </div>
+          </div>
+          <!-- Counter Item -->
+          <div class="col-md-3 col-sm-6 work-counter-widget text-center">
+            <div class="counter wow fadeInDown" data-wow-delay="1.2s">
+              <div class="icon"><i class="icon-heart"></i></div>
+              <!--<div class="counterUp">299</div>-->
+              <p>Creative Solutions</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Counter Area End-->
+
+    <!-- Contact Section Start -->
+    <section id="contact" class="section-padding">
+      <div class="contact-form">
+        <div class="container">
+            <div class="row contact-form-area wow fadeInUp" data-wow-delay="0.4s">
+                <div class="col-md-12 col-lg-12 col-sm-12">
+                    <h2>Contacts</h2>
+                    <div class="footer-right-area wow fadeIn">
+                        <div class="footer-right-contact">
+                            <a href="mailto:grapharet@gmail.com">
+                                <div class="single-contact">
+                                    <div class="contact-icon">
+                                        <i class="fa fa-envelope"></i>
+                                    </div>
+                                    <p>grapharet@gmail.com</p>
+                                </div>
+                            </a>
+                            <!--<a href="tel:+37433336277">
+                                <div class="single-contact">
+                                    <div class="contact-icon">
+                                        <i class="fa fa-phone"></i>
+                                    </div>
+                                    <p>+ (374) 33 33 62 77</p>
+                                </div>
+                            </a>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>   
+    </section>
+    <!-- Contact Section End -->
+
+    <!-- Footer Section Start -->
+    <footer class="footer-area section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="footer-text text-center wow fadeInDown" data-wow-delay="0.3s">
+              <ul class="social-icon">
+                <li>
+                  <a class="facebook" target="_blank" href="https://www.facebook.com/grapharet/"><i class="icon-social-facebook"></i></a>
+                </li>
+                <li>
+                  <a class="instagram" target="_blank" href="https://www.instagram.com/grapharet/"><i class="icon-social-instagram"></i></a>
+                </li>
+                <li>
+                  <a class="google" target="_blank" href="mailto:grapharet@gmail.com"><i class="icon-envelope"></i></a>
+                </li>
+              </ul>
+              <p>Copyright © <?php echo date("Y"); ?></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- Footer Section End -->
+
+    <!-- Go to Top Link -->
+    <a href="#" class="back-to-top">
+      <i class="icon-arrow-up"></i>
+    </a>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="assets/js/jquery-min.js"></script>
+    <!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
+
+    <script type="text/javascript" src="assets/js/popper.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="assets/js/jquery.mixitup.js"></script>
+    <!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mixitup/2.1.11/jquery.mixitup.min.js"></script>-->
+
+    <script type="text/javascript" src="assets/js/jquery.counterup.min.v1.js"></script>
+
+    <script type="text/javascript" src="assets/js/waypoints.min.js"></script>
+    <script type="text/javascript" src="assets/js/wow.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.nav.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.easing.min.js"></script>
+    <script type="text/javascript" src="assets/js/nivo-lightbox.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.slicknav.js"></script>
+
+    <script type="text/javascript" src="assets/js/base.js"></script>
+
+  </body>
+</html>
--- tt/65465.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Hanbragher
- * Date: 18.06.2026
- * Time: 22:43
- */
-gfg
