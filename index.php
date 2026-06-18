5e99af3ea9be95d8b3874accac86a0792f4cde2b
Hanbragher <hanbragher@gmail.com>
2026-06-19 00:32:22 +0400

    111

--- index.php
+++ index.php
@@ -32,21 +32,19 @@
     <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
 
       <!-- Yandex.Metrika counter -->
-      <script type="text/javascript" >
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      <script type="text/javascript">
+          (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
               m[i].l=1*new Date();
               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
 
-          ym(102979166, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true
-          });
+          ym(102979166, 'init', {clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
       </script>
       <noscript><div><img src="https://mc.yandex.ru/watch/102979166" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
       <!-- /Yandex.Metrika counter -->
+
 
   </head>
   <body>
